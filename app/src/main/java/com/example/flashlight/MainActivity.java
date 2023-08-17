09473f8d8fc523e6a7dec2a6e46d8fb3164ff55b
Javed <jk453396@gmail.com>
2023-08-17 18:08:48 +0530

    This is my First commit

---
+++ app/src/main/java/com/example/flashlight/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.flashlight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CameraManager cameraManager;
+    private String cameraId;
+    private boolean isFlashlightOn = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
+        try {
+            cameraId = cameraManager.getCameraIdList()[0]; // Use the first camera
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+        ToggleButton toggleButton = findViewById(R.id.toggleButton);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    turnOnFlashlight();
+                } else {
+                    turnOffFlashlight();
+                }
+            }
+        });
+    }
+
+    private void turnOnFlashlight() {
+        if (isCameraPermissionGranted()) {
+            try {
+                cameraManager.setTorchMode(cameraId, true);
+                isFlashlightOn = true;
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        } else {
+            requestCameraPermission();
+        }
+    }
+
+    private void turnOffFlashlight() {
+        try {
+            cameraManager.setTorchMode(cameraId, false);
+            isFlashlightOn = false;
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean isCameraPermissionGranted() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                turnOnFlashlight();
+            } else {
+                Toast.makeText(this, "Camera permission required to use flashlight", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (isFlashlightOn) {
+            turnOffFlashlight();
+        }
+    }
+}
